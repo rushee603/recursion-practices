231621d526d80ecefdf5d3dd95e5d6ecb5192429
Rushikesh <rushikesh.mule@plevenn.com>
2025-07-08 21:40:43 +0530

    simple factorial logic for example

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello worldd!");
+        int result = factorial(5);
+
+        System.out.println("result:" + result);
+    }
+
+    public static int factorial(int num) {
+
+        int value = 1;
+
+        for (int n = 1; n <= num; n++) {
+            value = value * n;
+        }
+        return value;
     }
 
 }
